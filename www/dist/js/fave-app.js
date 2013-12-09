585d7aaf5ee074e0d22fc58297899859907c2408
Søren Louv-Jansen <sorenlouv@gmail.com>
2013-12-08 22:51:00 -0800

    Moved Sunes stuff around

--- www/dist/js/fave-app.js
+++ www/dist/js/fave-app.js
@@ -125,67 +125,56 @@ app.factory('firebaseAuth', ['$q', 'safeApply', function($q, safeApply) {
   var userReadyDef = $q.defer();
 
   // Create firebase authentication object
-  var firebaseAuth = {
-    userReady: userReadyDef.promise,
+  var usersRef = new Firebase("https://fave.firebaseio.com/users");
+  var auth = new FirebaseSimpleLogin(usersRef, onAuthenticationChange.bind(this));
 
-    login: function(accessToken){
-      this.usersRef = new Firebase("https://fave.firebaseio.com/users");
-      this.auth = new FirebaseSimpleLogin(this.usersRef, this.onAuthenticationChange.bind(this));
+  /*
+   * Attempt to login user
+   * Return promise which will be resolved in "onAuthenticationChange" if login successful
+   ****************************************/
+  var login = function(accessToken){
+    this.auth.login('facebook', {
+      access_token: accessToken
+    });
 
-      this.auth.login('facebook', {
-        access_token: accessToken
+    return userReadyDef.promise;
+  };
+
+  // onChange: Authentication to Firebase
+  var onAuthenticationChange = function(error, facebookUser) {
+    if(facebookUser){
+      this.createCurrentUser(facebookUser).then(function(){
+        userReadyDef.resolve();
       });
-    },
-
-    getOrCreateCurrentUser: function(facebookUser){
-      var deferred = $q.defer();
-      var activeUserRef = firebaseAuth.usersRef.child(facebookUser.id);
-      activeUserRef.once('value', function(activeUserSnapshot){
-
-        // Create user if not exists, or fetch from DB
-        var activeUser;
-        if(activeUserSnapshot.val() === null){
-          // user does not exist
-          activeUser = {
-            id: facebookUser.id,
-            name: facebookUser.name,
-            first_name: facebookUser.first_name,
-            last_name: facebookUser.last_name,
-            email: facebookUser.email
-          };
-          activeUserRef.set(activeUser);
-        }else{
-          // user already exists
-          activeUser = activeUserSnapshot.val();
-        }
-
-        deferred.resolve(activeUser);
-      });
-
-      return deferred.promise;
-    },
-
-    onAuthenticationChange: function(error, facebookUser) {
-      if(error){
-        // auth error
-        console.log("Authentication error", error);
-        this.userReadyDef.reject(error);
-
-      }else if(facebookUser){
-        // user signed in with Facebook
-        console.log("User signed in!");
-        this.getOrCreateCurrentUser(facebookUser).then(function(activeUser){
-          userReadyDef.resolve(activeUser);
-        });
-
-      }else{
-        // user signed out
-        // userReadyDef.reject("User signed out");
-      }
     }
   };
 
-  return firebaseAuth;
+  /*
+   * Create user after successful login, if he doesn't exist
+   ****************************************/
+  var createCurrentUser = function(facebookUser){
+    var createUserDef = $q.defer();
+    var activeUserRef = usersRef.child(facebookUser.id);
+    activeUserRef.once('value', function(activeUserSnapshot){
+
+      // Create user if not exists
+      if(activeUserSnapshot.val() === null){
+        activeUserRef.set({
+          id: facebookUser.id,
+          name: facebookUser.name,
+          first_name: facebookUser.first_name,
+          last_name: facebookUser.last_name,
+          email: facebookUser.email
+        });
+      }
+
+      createUserDef.resolve();
+    });
+
+    return createUserDef.promise;
+  };
+
+  return login;
 }]);
 angular.module('safeApply',[]).factory('safeApply', [function($rootScope) {
     'use strict';
