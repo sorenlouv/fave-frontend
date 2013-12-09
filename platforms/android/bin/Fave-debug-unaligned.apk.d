f46f937c403cdf7aac065e4c04010c6eedfd8024
Søren Louv-Jansen <sorenlouv@gmail.com>
2013-12-09 00:54:08 -0800

    Removed built files from repo

--- platforms/android/bin/Fave-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/srv/phonegap-dev/projects/faveapp/platforms/android/bin/Fave-debug-unaligned.apk : \
-/srv/phonegap-dev/projects/faveapp/platforms/android/bin/Fave.ap_ \
-/srv/phonegap-dev/projects/faveapp/platforms/android/bin/classes.dex \
-/srv/phonegap-dev/projects/faveapp/platforms/android/libs/cordova-3.2.0.jar \
