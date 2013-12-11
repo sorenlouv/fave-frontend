c4bd780039290ef8b8066f094bb081a756422c7f
Søren Louv-Jansen <sorenlouv@gmail.com>
2013-12-11 01:38:01 -0800

    Camera is fucking working!!!

--- www/src/pages/home/home.js
+++ www/src/pages/home/home.js
@@ -1,33 +1,47 @@
 app.controller('homeController', ['$scope', 'facebook', 'safeApply', function ($scope, facebook, safeApply) {
   'use strict';
 
-  facebook.userReady.then(function(){
-    FB.api('/me', function(activeUser) {
-      safeApply($scope, function(){
-        $scope.activeUser = activeUser;
-      });
-    });
-  });
+  // facebook.userReady.then(function(){
+  //   FB.api('/me', function(activeUser) {
+  //     safeApply($scope, function(){
+  //       $scope.activeUser = activeUser;
+  //     });
+  //   });
+  // });
 
   /*
    * Click events
    ********************************************/
 
-  $scope.login = function(){
-    facebook.sdkReady.then(function(){
-      FB.login(null, { scope: "email" });
-    });
-  };
+  // $scope.login = function(){
+  //   facebook.sdkReady.then(function(){
+  //     FB.login(null, { scope: "email" });
+  //   });
+  // };
 
-  $scope.me = function(path){
-    facebook.sdkReady.then(function(){
-      FB.api('/me', function(response) {
-        alert("Hej " + response.name);
+  // $scope.me = function(path){
+  //   facebook.sdkReady.then(function(){
+  //     FB.api('/me', function(response) {
+  //       alert("Hej " + response.name);
+  //     });
+  //   });
+  // };
+
+  $scope.captureImage = function(){
+    navigator.camera.getPicture( function(image){
+      // success
+      $scope.$apply(function(){
+        $scope.imageResponse = image;
       });
+    }, function(error){
+      // error
+      alert("error");
+      $scope.imageResponse = error;
+    }, {
+      quality: 50,
+      destinationType: Camera.DestinationType.FILE_URI,
+      allowEdit: true
     });
   };
 
-  $scope.action = function() {
-    console.log("hey");
-  };
 }]);
